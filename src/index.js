7b218752c5c3aa4a146032e2eaebdfff900e49e4
bjoluc <mail@bjoluc.de>
2020-04-23 12:20:56 +0200

    feat(CLI): Add update notification using `update-notifier`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+"use strict";
 
+const updateNotifier = require("update-notifier");
 const cli = require("./cli");
+const pkg = require("../package.json");
+
+// Notify about updates at the end
+updateNotifier({ pkg }).notify();
 
 // Run the program
 cli.program.parseAsync(process.argv);
